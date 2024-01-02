5b1b6989a2e8028b35af7319d2d0c6af680e2392
Iso Elk <105553166+ismaikl@users.noreply.github.com>
2024-01-03 00:55:39 +0100

    back

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,26 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * check_cycle - checks if singly linked list is a cycle
- * Return: 0 if no cycle, 1 is yes
-*/
+ * check_cycle - function checks if a singly linked list has a cycle in it.
+ * @list: pointer to the beginning of the node
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = list;
-	listint_t *slow = list;
+	listint_t *current, *check;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
+	current = list;
+	check = current->next;
 
-	while (1)
+	while (current != NULL && check->next != NULL
+		&& check->next->next != NULL)
 	{
-		/*traverse through nodes as long as linked list node exists*/
-		if (fast->next != NULL && fast->next->next != NULL)
-		{
-			fast = fast->next->next;
-			slow = slow->next;
-
-			if (fast == slow) /*if nodes match, cycle found*/
-				return (1);
-		}
-		else
-			return (0);
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
 	}
-
+	return (0);
 }
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,3 +1,1 @@
-#python_hello_world
-
-This is an introduction to python
+We Start high level programming now. Python is a high level programming language and most of what we do here is going to be python.
