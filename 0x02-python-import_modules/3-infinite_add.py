b46a566f025acc83c2c7f7c3e6cd888f5ebc85ed
Iso Elk <105553166+ismaikl@users.noreply.github.com>
2024-01-04 18:19:33 +0100

    tag tag

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+import sys
+
+def main():
+    # Initialize the sum to 0
+    total = 0
+
+    # Iterate over the arguments (excluding the script name)
+    for arg in sys.argv[1:]:
+        # Convert the argument to an integer and add it to the total
+        total += int(arg)
+
+    # Print the total
+    print(total)
+
+# Ensure the script doesn't run when imported
 if __name__ == "__main__":
-    """ Add all arguments."""
-    import sys
-
-    result = 0
-
-    for i in range(len(sys.argv) - 1):
-        result += (int(sys.argv[i + 1]))
-    print("{:d}".format(result))
+    main()
